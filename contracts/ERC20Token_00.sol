acce9e5d87d006d6be118006e0c66bd294725d0b
hondo-s <shunsuke.hondo@gmail.com>
2018-08-26 14:04:02 +0900

    fix

--- contracts/ERC20Token_00.sol
+++ contracts/ERC20Token_00.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.24;
 contract HelloWorld {
     string public message;
 
-    function setName(string _message) public {
+    function setMessage(string _message) public {
         message = _message;
     }
 }
