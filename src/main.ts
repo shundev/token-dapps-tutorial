567f13d01ef8ffb886849e0126c9269630db16e6
hondo-s <shunsuke.hondo@gmail.com>
2019-02-11 11:47:48 +0900

    Initialize with Typescript & Vue

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import Index from './Index.vue';
-
-Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<Index/>',
-  components: { Index },
+  data: {
+    message: 'Hello, TS!',
+  },
 });
